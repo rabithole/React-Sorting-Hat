13ac259b44fbc71764ab95e2323b57279959b08d
Robert Kulp <pedalinon@gmail.com>
2020-01-15 19:21:58 -0800

    Project started

---
+++ react-sorting-hat/src/components/LandingPage.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+
+
+const LandingPage = () => {
+	// const cart = useContext();
+
+	return (
+		<div className="landingPage">
+			<h1>Landing Page</h1>
+			<button>Begin Sorting</button>
+		</div>
+	);
+};
+
+export default LandingPage;
